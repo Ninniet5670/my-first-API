0281237c04a02cfb04881dbc638a8b8b84bc3dae
Marco Proença  / Administrativo / Matriz / Deltamaq <marco.proenca@deltamaq.com.br>
2024-05-27 16:53:17 -0300

    added post route and now serveer running through python

--- my_first_api/main.py
+++ my_first_api/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from my_first_api.schemas import User
+import uvicorn
 
-app = FastAPI()
+app = FastAPI(title='my-first-API')
 
 @app.get('/')
 def root():
-    return 'Hello, World!'
+    return 'Hello, World! You may go to /docs to see further routes and test them'
+
+@app.post('/user')
+def user_page(user_info: User):
+    return f'Hello, {user_info.username}, born in {user_info.birthday}'
+
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run('my_first_api.main:app', log_level='info', reload=True)

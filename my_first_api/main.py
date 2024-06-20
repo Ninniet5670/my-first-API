2af15c9ac903c4140daa7c5ff515c2176717d955
PROENÇA, Marco <contato.marcoproenca@gmail.com>
2024-06-20 17:39:06 -0300

    routes logic and exception handling implemented

--- my_first_api/main.py
+++ my_first_api/main.py
@@ -12,16 +12,16 @@ tags_metadata = [
         "description": "Home route of the project",
     },
     {
-        "name": "view sale",
-        "description": "Return one sale information. 'So _fancy_ they have their own docs.'",
-    },
-    {
         "name": "view all sales",
         "description": "Return all sales prices inside the CRUD.",
     },
     {
         "name": "sasalele",
         "description": "A print with users from body parameter.",
+    },
+    {
+        "name": "view sale",
+        "description": "Return one sale information. 'So _fancy_ they have their own docs.'",
     },
     {
         "name": "create sale",
@@ -55,10 +55,6 @@ with open('database/sales.json', 'r+') as arquivo:
 def root():
     return {'response': 'Hello, World! You may go to /docs ou /redoc to see further routes and test them'}
 
-@app.get('/sale/{sale_id}', tags=["view sale"])
-def view_sale(sale_id: str):
-    return sales[sale_id]
-
 @app.get('/sales', tags=["view all sales"])
 def view_all_sales():
     return sales
@@ -67,32 +63,43 @@ def view_all_sales():
 def another_view_sale(sale_info: Sale):
     return {'response': f'You just bought {sale_info.quantity} {sale_info.item} for a full R${sale_info.unit_price}? Anyways heres your id: {sale_info.id}'}
 
+@app.get('/sale/{sale_id}', tags=["view sale"])
+def view_sale(sale_id: str):
+    try:
+        return sales[sale_id]
+    except:
+        raise HTTPException(status_code=404, detail='Not any sales with given ID')
+
 @app.post('/create_sale/', tags=["create sale"])
 def create_sale(sale_info: Sale):
-    sales.append(sale_info)
-    return sale_info
+    for sale in sales:
+        print(sale)
+        if sale['id'] == sale_info.id:
+            raise HTTPException(status_code=409, detail='Sale with given ID already inside JSON')
+    else:
+        sales.append(sale_info.model_dump())
+        return {"response": 'Sale created successfully'}
 
 @app.put('/update_sale/{sale_id}', tags=["update sale"])
 def update_sale(sale_id:int, sale_info: Sale):
     for i, j in enumerate(sales):
         if j['id'] == sale_id:
-            sales[i] = sale_info
-            break
+            for sale in sales:
+                if sale_info.id == sale['id'] and sale_id != sale['id']:
+                    raise HTTPException(status_code=409, detail='Sale with given ID already inside JSON')
+            sales[i] = sale_info.model_dump()
+            return {"response": f'Sale ID: {sale_id} updated successfully'}
     else:
         raise HTTPException(status_code=404, detail='Not any sales with given ID')
     
-    return {"response": f'Sale ID: {sale_id} updated successfully'}
-
 @app.delete('/delete_sale/{sale_id}', tags=["delete sale"])
 def delete_sale(sale_id:int):
     for i, j in enumerate(sales):
         if j['id'] == sale_id:
             del sales[i]
-            break
+            return {"response": f'Sale ID: {sale_id} deleted successfully'}
     else:
         raise HTTPException(status_code=404, detail='Not any sales with given ID')
-
-    return {"response": f'Sale ID: {sale_id} deleted successfully'}
 
 
 if __name__ == '__main__':
